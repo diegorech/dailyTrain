28a67c1c745ea1e71c0973ef172c86d935dcce0e
Diego Rech <diegorech@gmail.com>
2019-09-19 19:57:09 -0300

    First commit - simple About animation

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+let introCanvas = document.querySelector(".intro"); 
+
+
+let aboutBtn = document.querySelector("#aboutBtn").addEventListener('click', () => {
+    let contains = introCanvas.classList.com
+    
+    
+    introCanvas.classList.toggle('show')
+
+   return console.log(introCanvas.classList)
+})
